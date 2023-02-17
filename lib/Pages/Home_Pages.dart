3e0dc7956be03f77694996044629b99cd119064c
Luk Gutierrez <lucianogutierrezagustin@gmail.com>
2023-02-17 16:47:28 -0300

    finishdrawerpage

--- lib/Pages/Home_Pages.dart
+++ lib/Pages/Home_Pages.dart
@@ -38,7 +38,76 @@ class HomePage extends StatelessWidget {
       //         label: "TINA"),
       //   ],
       // ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: Column(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: Image(image: AssetImage("assets/miargentina.png")),
+                ),
+                ListTile(
+                  title: Row(
+                    children: [
+                      Text(
+                        "Jorge Lopez Moralez",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Container(
+                        width: 25,
+                        height: 25,
+                        child: Image(image: AssetImage("assets/check.png")),
+                      )
+                    ],
+                  ),
+                  subtitle: Text(
+                    "459587345",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 18,
+                      height: 10,
+                    ),
+                    Text(
+                      "Identidad Validada",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            Divider(
+              height: 10.0,
+              color: Colors.black,
+            ),
+            //////////////////////////////////////////////////////////
+            listicon("Mi perfil", "assets/profile.png"),
+            listicon("Hijos/as asociados", "assets/family.png"),
+            listicon("Notificaciones", "assets/timber.png"),
+            Divider(
+              height: 10.0,
+              color: Colors.black,
+            ),
+            /////////////////////////////////////////////////////////////
+            listicon("Acerca de esta aplicación", "assets/information.png"),
+            listicon("Términos y condiciones", "assets/c.png"),
+            ///////////////////////////////////////////////////////////////
+            Divider(
+              height: 10.0,
+              color: Colors.black,
+            ),
+            listicon("Cerrar sesión", "assets/exit.png"),
+            listicon("Volver al inicio", "assets/hauseicon.png")
+          ],
+        ),
+      ),
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Color(0xFFF2CAEE8),
@@ -57,4 +126,18 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+listicon(name, iconts) {
+  return ListTile(
+    title: Text(name,
+        style: TextStyle(
+          fontSize: 15.0,
+          fontWeight: FontWeight.w500,
+        )),
+    leading: Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Image(image: AssetImage(iconts)),
+    ),
+  );
 }
