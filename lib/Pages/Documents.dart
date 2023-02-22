1c6e1429a907fac766488909b1e7d10a028024a7
Luk Gutierrez <lucianogutierrezagustin@gmail.com>
2023-02-22 17:16:42 -0300

    finish_edit_pagedocuments

--- lib/Pages/Documents.dart
+++ lib/Pages/Documents.dart
@@ -20,12 +20,63 @@ class _DocumentsState extends State<Documents> {
       body: Column(
         children: [
           ExpansionPanelList(
-            expansionCallback: (int index, bool isExpanded) {
+            expansionCallback: (int panelIndex, bool isExpanded) {
               setState(() {
                 _expanded = !_expanded;
               });
             },
             children: [
+              //////////////////////////PANEL DNI DIGITAL/////////////////////////////////
+              ExpansionPanel(
+                  headerBuilder: (context, isExpanded) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 18,
+                          height: 18,
+                        ),
+                        SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Image(
+                                image: AssetImage("assets/documents.jpg"))),
+                        Text(
+                          "   DNI Digital: 42563675",
+                          style: TextStyle(
+                              fontSize: 19, fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    );
+                  },
+                  body: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Container(
+                          height: 50,
+                          width: 300,
+                          child: Center(child: Text('VER MI DNI DIGITAL'))),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color?>(
+                          (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.pressed)) {
+                              return Color(0xFFF2CAEE8);
+                            }
+                            return Color(0xFFF2CAEE8);
+                          },
+                        ),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  isExpanded: _expanded),
+              ///////////////////////////////////////////////////////////////////////////
               ExpansionPanel(
                 headerBuilder: (context, isExpanded) {
                   return Image(image: AssetImage("assets/image7.jpg"));
@@ -35,28 +86,27 @@ class _DocumentsState extends State<Documents> {
                   height: 100,
                   color: Colors.amber,
                 ),
-                isExpanded: _expanded,
               ),
               ExpansionPanel(
-                  headerBuilder: (context, isExpanded) {
-                    return Image(image: AssetImage("assets/image2.jpg"));
-                  },
-                  body: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.amber,
-                  ),
-                  isExpanded: _expanded),
+                headerBuilder: (context, isExpanded) {
+                  return Image(image: AssetImage("assets/image2.jpg"));
+                },
+                body: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                ),
+              ),
               ExpansionPanel(
-                  headerBuilder: (context, isExpanded) {
-                    return Image(image: AssetImage("assets/image3.jpg"));
-                  },
-                  body: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.amber,
-                  ),
-                  isExpanded: _expanded)
+                headerBuilder: (context, isExpanded) {
+                  return Image(image: AssetImage("assets/image3.jpg"));
+                },
+                body: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                ),
+              )
             ],
           ),
         ],
