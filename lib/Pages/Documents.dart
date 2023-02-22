d6f54c64f37760cae0de4c9051ac72a5ded5d351
Luk Gutierrez <lucianogutierrezagustin@gmail.com>
2023-02-22 15:33:42 -0300

    editpage

--- lib/Pages/Documents.dart
+++ lib/Pages/Documents.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class Documents extends StatelessWidget {
+class Documents extends StatefulWidget {
   const Documents({Key? key}) : super(key: key);
 
+  @override
+  State<Documents> createState() => _DocumentsState();
+}
+
+class _DocumentsState extends State<Documents> {
+  bool _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,17 +20,23 @@ class Documents extends StatelessWidget {
       body: Column(
         children: [
           ExpansionPanelList(
+            expansionCallback: (int index, bool isExpanded) {
+              setState(() {
+                _expanded = !_expanded;
+              });
+            },
             children: [
               ExpansionPanel(
-                  headerBuilder: (context, isExpanded) {
-                    return Image(image: AssetImage("assets/image7.jpg"));
-                  },
-                  body: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.amber,
-                  ),
-                  isExpanded: false),
+                headerBuilder: (context, isExpanded) {
+                  return Image(image: AssetImage("assets/image7.jpg"));
+                },
+                body: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                ),
+                isExpanded: _expanded,
+              ),
               ExpansionPanel(
                   headerBuilder: (context, isExpanded) {
                     return Image(image: AssetImage("assets/image2.jpg"));
@@ -34,7 +46,7 @@ class Documents extends StatelessWidget {
                     height: 100,
                     color: Colors.amber,
                   ),
-                  isExpanded: false),
+                  isExpanded: _expanded),
               ExpansionPanel(
                   headerBuilder: (context, isExpanded) {
                     return Image(image: AssetImage("assets/image3.jpg"));
@@ -44,7 +56,7 @@ class Documents extends StatelessWidget {
                     height: 100,
                     color: Colors.amber,
                   ),
-                  isExpanded: false),
+                  isExpanded: _expanded)
             ],
           ),
         ],
